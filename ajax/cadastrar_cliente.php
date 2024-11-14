ae8a35cc86a6e56b4f15df0234ba1a0e0ce01504
kerlonfernandes <kerlon1221@gmail.com>
2024-11-14 13:20:49 -0300

    anexos

--- ajax/cadastrar_cliente.php
+++ ajax/cadastrar_cliente.php
@@ -127,8 +127,12 @@ try {
                         throw new Exception('O arquivo é muito grande. O tamanho máximo permitido é 5MB.');
                     }
 
-                    $dir = '/app/uploads/pdf/';
-                    $uploadDir = "..". $dir;
+                    $uploadDir = "../app/uploads/pdf/";
+                    if (!is_dir($uploadDir)) {
+                        mkdir($uploadDir, 0777, true);
+                    }
+
+                    $filePath = $uploadDir . uniqid() . "_" . basename($fileName);
                     // Garantir que o diretório exista
                     if (!is_dir($uploadDir)) {
                         mkdir($uploadDir, 0777, true);
